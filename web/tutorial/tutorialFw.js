b98dcd6d758c89352899d9389129a20aa286a46a
Max Bugay <MaxBugay1@gmail.com>
2018-04-05 00:58:47 -0400

    Working on tutorial

--- web/tutorial/tutorialFw.js
+++ web/tutorial/tutorialFw.js
@@ -1,7 +1,37 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+function makeSlideshow(color, numSlides) {
+            
+            var slideShowObj = {};
+            slideShowObj.divID = color;
+            slideShowObj.placeContent = function(numSlides) {
+                document.getElementById(contentObj.divID).innerHTML = numSlides;
+            }
+            var slideIndex = 1;
+            showSlides(slideIndex);
+
+            function plusSlides(n) {
+                showSlides(slideIndex += n);
+            }
+
+            function currentSlide(n) {
+                showSlides(slideIndex = n);
+            }
+
+            function showSlides(n) {
+            var i;
+            var slides = document.getElementsByClassName(color);
+            var dots = document.getElementsByClassName("dot");
+            if (n > slides.length) {slideIndex = 1}    
+                if (n < 1) {slideIndex = slides.length}
+                    for (i = 0; i < slides.length; i++) {
+                        slides[i].style.display = "none";  
+            }
+            for (i = 0; i < dots.length; i++) {
+                dots[i].className = dots[i].className.replace(" active", "");
+            }
+            slides[slideIndex-1].style.display = "block";  
+            dots[slideIndex-1].className += " active";
+            }
+}
+
 
 
