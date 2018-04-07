c73fce8825d46968592d49ed28e5c67f9da70c78
Max Bugay <MaxBugay1@gmail.com>
2018-04-07 03:47:58 -0400

    Tutorial Working

--- web/tutorial/tutorialFw.js
+++ web/tutorial/tutorialFw.js
@@ -1,27 +1,25 @@
-
-var imageNumber = 0;
-
-var colorArray = ['Platinum', 'Burgundy', 'Cobalt', 'Gold'];
-
-var images = ["../pics/tutorialpics/Surface-Laptop-Plat1.jpg", "../pics/tutorialpics/Surface-Laptop-Plat2.jpg", "../pics/tutorialpics/Surface-Laptop-Plat3.jpg"];
-
-var imageLength = images.length - 1;
-
-var slideshowFW = {};
+var slideShowFW = {};
 
 (function () {
+    
+    var slideIndex = 0;
+    var slideShow;
+    var colorArray = ['Platinum', 'Burgundy', 'Cobalt', 'Gold'];
+    var slides = ["../pics/tutorialpics/Surface-Laptop-Plat1.jpg", "../pics/tutorialpics/Surface-Laptop-Plat2.jpg", "../pics/tutorialpics/Surface-Laptop-Plat3.jpg", "../pics/tutorialpics/Surface-Laptop-Plat4.jpg", "../pics/tutorialpics/Surface-Laptop-Plat5.jpg"];
+    var slidesLength = slides.length - 1;
+    var interval;
     
     function $(element) {
         return document.getElementById(element);
     }
     
-    slideshowFW.makeSlideShow = function (params) {
+    slideShowFW.makeSlideShow = function (params) {
         if (!params || !params.id) {
             alert("makeSlideShow must be passed an object with 'id' property.");
             return;
         }
         
-        var slideShow = $(params.id);
+        slideShow = $(params.id);
         
         if (!slideShow) {
             alert("makeSlideShow must be passed an object with an 'id' property " +
@@ -31,49 +29,54 @@ var slideshowFW = {};
         
         //Default to Platinum Slides if none are passed
         if (!params.slidePics || !Array.isArray(params.slidePics)) {
-            params.slidePics = ["../pics/tutorialpics/Surface-Laptop-Plat1.jpg", "../pics/tutorialpics/Surface-Laptop-Plat2.jpg", "../pics/tutorialpics/Surface-Laptop-Plat3.jpg"];
+            params.slidePics = ["../pics/tutorialpics/Surface-Laptop-Plat1.jpg", "../pics/tutorialpics/Surface-Laptop-Plat2.jpg", "../pics/tutorialpics/Surface-Laptop-Plat3.jpg", "../pics/tutorialpics/Surface-Laptop-Plat4.jpg", "../pics/tutorialpics/Surface-Laptop-Plat5.jpg"];
         }
             
-        slideShow.changeColor = function (c) {
-            if (c === colorArray[0]) {
-                params.slidePics = ["../pics/tutorialpics/Surface-Laptop-Plat1.jpg", "../pics/tutorialpics/Surface-Laptop-Plat2.jpg", "../pics/tutorialpics/Surface-Laptop-Plat3.jpg"];
+        slideShow.changeColor = function (color) {
+            if (color === colorArray[0]) {
+                params.slidePics = ["../pics/tutorialpics/Surface-Laptop-Plat1.jpg", "../pics/tutorialpics/Surface-Laptop-Plat2.jpg", "../pics/tutorialpics/Surface-Laptop-Plat3.jpg", "../pics/tutorialpics/Surface-Laptop-Plat4.jpg", "../pics/tutorialpics/Surface-Laptop-Plat5.jpg"];
             }
-            else if (c === colorArray[1]) {
-                params.slidePics = ["../pics/tutorialpics/Surface-Laptop-Burg1.jpg", "../pics/tutorialpics/Surface-Laptop-Burg2.jpg", "../pics/tutorialpics/Surface-Laptop-Burg3.jpg"];
+            else if (color === colorArray[1]) {
+                params.slidePics = ["../pics/tutorialpics/Surface-Laptop-Burg1.jpg", "../pics/tutorialpics/Surface-Laptop-Burg2.jpg", "../pics/tutorialpics/Surface-Laptop-Burg3.jpg", "../pics/tutorialpics/Surface-Laptop-Burg4.jpg", "../pics/tutorialpics/Surface-Laptop-Burg5.jpg"];
             }
-            else if (c === colorArray[2]) {
-                params.slidePics = ["../pics/tutorialpics/Surface-Laptop-Cob1.jpg", "../pics/tutorialpics/Surface-Laptop-Cob2.jpg", "../pics/tutorialpics/Surface-Laptop-Cob3.jpg"];
+            else if (color === colorArray[2]) {
+                params.slidePics = ["../pics/tutorialpics/Surface-Laptop-Cob1.jpg", "../pics/tutorialpics/Surface-Laptop-Cob2.jpg", "../pics/tutorialpics/Surface-Laptop-Cob3.jpg", "../pics/tutorialpics/Surface-Laptop-Cob4.jpg", "../pics/tutorialpics/Surface-Laptop-Cob5.jpg"];
             }
-            else if (c === colorArray[3]) {
-                params.slidePics = ["../pics/tutorialpics/Surface-Laptop-Gold1.jpg", "../pics/tutorialpics/Surface-Laptop-Gold2.jpg", "../pics/tutorialpics/Surface-Laptop-Gold3.jpg"];
+            else if (color === colorArray[3]) {
+                params.slidePics = ["../pics/tutorialpics/Surface-Laptop-Gold1.jpg", "../pics/tutorialpics/Surface-Laptop-Gold2.jpg", "../pics/tutorialpics/Surface-Laptop-Gold3.jpg", "../pics/tutorialpics/Surface-Laptop-Gold4.jpg", "../pics/tutorialpics/Surface-Laptop-Gold5.jpg"];
             }
-            images = params.slidePics;
+            slides = params.slidePics;
+            imageLength = slides.length - 1;
         };    
         
         slideShow.style.textAlign = "center";
         slideShow.style.padding = 10 + "px";
         
-        imageLength = params.slidePics.length - 1;
-        
-        slideShow.changeImage = function(n) {
-            imageNumber += n;
+        slideShow.changeSlide = function(n) {
+            slideIndex += n;
             
             //Restart slideshow at end of it
-            if (imageNumber > imageLength) {
-                imageNumber = 0;
+            if (slideIndex > slidesLength) {
+                slideIndex = 0;
             }
             //Back on slide 1 to end of array
-            if (imageNumber < 0) {
-                imageNumber = imageLength;
+            if (slideIndex < 0) {
+                slideIndex = slidesLength;
             }
-            document.getElementById("slideshow").src = images[imageNumber];
-            
-            //return false;
+            document.getElementById(params.id).src = slides[slideIndex];
         };
+        
+        slideShow.startSlideShow = function() {
+             interval = setInterval("changeSlide(1)", 2000);
+             return interval;
+        };
+        
+        slideShow.stopSlideShow = function() {
+             clearInterval(interval);
+             return interval;
+        };
+        
         console.log(slideShow);
         return slideShow;
     };
 })();
-
-
-
