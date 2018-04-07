06cc1d45bf2cc9869e9030b34c0ee8eeee1b94f4
Max Bugay <MaxBugay1@gmail.com>
2018-04-06 20:22:08 -0400

    Working on tutorial

--- web/tutorial/tutorialFw.js
+++ web/tutorial/tutorialFw.js
@@ -1,21 +1,28 @@
 
-function makeSlideshow(colorA, numSlides) {
+function makeSlideshow(slides) {
+    
+            var slidePics = slides;
+            console.log(slides);
             
             var slideshowObj = {};
-            var colorArray = colorA;
+            //var colorArray = colorA;
             //slideShowObj.divID = color;
-            var slideIndex = numSlides;
-            
+            var slideIndex = 1;
             
             showSlides(slideIndex);
             
-            slideshowObj.changeSlideshow = function(color){
+            /*slideshowObj.changeSlideshow = function(color){
                 for (var i = 0; i < colorArray.length; i++) {
                     if (colorArray[i] === color) {
                         showSlides(slideIndex, color);
                     }
                 }
             }
+    
+            if (!slidePics || !Array.isArray(slidePics) {
+                slidePics = ["../pics/tutorialpics/Surface-Laptop-Plat1.jpg", "../pics/tutorialpics/Surface-Laptop-Plat2.jpg", "../pics/tutorialpics/Surface-Laptop-Plat3.jpg"];
+                console.log(slidePics);
+            }*/
 
             function plusSlides(n) {
                 showSlides(slideIndex += n);
@@ -25,13 +32,18 @@ function makeSlideshow(colorA, numSlides) {
                 showSlides(slideIndex = n);
             }
 
-            function showSlides(n, color) {
+            function showSlides(n) {
             var i;
-            var slides = document.getElementById('slideshowCont').children;
-            var slides2 = document.getElementById('slideshowCont2').children;
-            //var slides = document.getElementsByClassName('Platinum');
+            var elem;
+            //elem.src = '../pics/tutorialpics/Surface-Laptop-Plat1.jpg';
+            //console.log(elem.src);
+            
+            //document.getElementById("slideshowCont").appendChild(elem);
+            //var slides = document.getElementById('slideshowCont').children;
+            //var slides2 = document.getElementById('slideshowCont2').children;
+            //slides = document.getElementsByClassName("Platinum");
             console.log(slides);
-            console.log(slides2);
+            //console.log(slides2);
             var dots = document.getElementsByClassName("dot");
             if (n > slides.length) {slideIndex = 1}    
                 if (n < 1) {slideIndex = slides.length}
